346f8c069fd0075c6d9f466c83e04a12d81e09c6
Luka Lončar <lloncar.vz@gmail.com>
2025-12-22 19:30:23 +0530

    created Dog

---
+++ Assets/Scripts/Dogs/Runtime/Model/Dog.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace GimGim.Dogs {
+    public class Dog {
+        [SerializeField]
+        public DogProfile profile;
+        [SerializeField] 
+        public string name;
+        
+
+    }
+}
